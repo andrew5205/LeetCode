8d270bf4dc3e03da44f1e5b30c09cb974fc99caf
Andrew5205 <andrewchung11@gmail.com>
2020-06-29 14:27:35 -0700

    Update 20_Valid_Parentheses.js

--- 11-20/20_Valid_Parentheses.js
+++ 11-20/20_Valid_Parentheses.js
@@ -150,6 +150,28 @@ console.log(isValid("{[]}"));
 
 
 // ***************************************************************************** //
+function isValid(s) {
+    let map = {
+        ")": "(",
+        "]": "[",
+        "}": "{"
+    }
+    let arr = [];
+    for(let i = 0; i < s.length; i ++){
+        if(s[i] === "(" || s[i] === "[" || s[i] === "{"){
+            arr.push(s[i]);
+        }
+        else{
+            if(arr[arr.length - 1] === map[s[i]]){
+                arr.pop();
+            }
+            else return false;
+        }
+    }
+    return arr.length === 0 ? true : false;
+};
+
+// ***************************************************************************** //
 console.log('approach 3');
 
 
@@ -174,4 +196,5 @@ var isValid = function(s) {
     
     return stack.length === 0;
 };
+
 
